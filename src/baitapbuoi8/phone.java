a9fb6d1234e8182e960f73b39044e8faa9931f41
This PC <aaseae@dafaf.com>
2022-09-24 17:58:47 +0700

    Phung dinh duy

---
+++ src/baitapbuoi8/phone.java
@@ -0,0 +1,14 @@
+public abstract class phone {
+    public phone() {
+    }
+
+    abstract void insertPhone(String var1, String var2);
+
+    abstract void removePhone(String var1);
+
+    abstract void updatePhone(String var1, String var2);
+
+    abstract void searchPhone(String var1);
+
+    abstract void sort();
+}
